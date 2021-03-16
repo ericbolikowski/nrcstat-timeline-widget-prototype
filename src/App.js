8b662eb950ae4179d07564e343fdbfd150335444
ericbolikowski <eric@binarylights.com>
2021-03-16 14:33:55 +0100

    strip down to base template

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import './styles.css';
-import { Widget } from './Widget';
-
-import { samplePictogramWidgetObjects } from './sample-widget-objects';
+import React from "react";
+import "./styles.css";
+import { Widget } from "./Widget";
 
 export default function App() {
   return (
     <div className="App">
-      {samplePictogramWidgetObjects.map((w, key) => (
-        <>
-          <Widget key={key} {...w} />
-        </>
-      ))}
+      <Widget />
     </div>
   );
 }
--- src/Widget.js
+++ src/Widget.js
@@ -1,88 +1,14 @@
-import React from 'react';
-import {
-  BackgroundColorKey,
-  BackgroundColorToIconBaseColorMap,
-  DataColor,
-  IconBaseColor,
-} from './config';
-import {
-  Camp,
-  Education,
-  Food,
-  Legal,
-  Shelter,
-  WASH,
-  PeopleFemale,
-  PeopleMale,
-  PeopleLittleBoy,
-  PeopleLittleGirl,
-  PeopleSchoolGirl,
-  PeopleSchoolBoy,
-  PeopleChildren,
-  PeopleRefugeeFamily,
-  PeopleRefugeeFamilyAlt,
-  PeopleRefugeesRunning,
-} from './icons/index.js';
-import { isMobileDevice } from './isMobileDevice';
-import './Widget.scss';
+import React from "react";
 
-const WidgetIconMap = {
-  PeopleFemale,
-  PeopleMale,
-  PeopleLittleBoy,
-  PeopleLittleGirl,
-  PeopleSchoolGirl,
-  PeopleSchoolBoy,
-  PeopleChildren,
-  PeopleRefugeeFamily,
-  PeopleRefugeeFamilyAlt,
-  PeopleRefugeesRunning,
-  Camp,
-  Education,
-  Food,
-  Legal,
-  Shelter,
-  WASH,
-};
+import { isMobileDevice } from "./isMobileDevice";
+import "./Widget.scss";
 
 const isMobile = isMobileDevice();
 
-export function Widget({
-  title,
-  subtitle,
-  source,
-  backgroundColor = BackgroundColorKey.White,
-  sections,
-}) {
-  const iconBaseColor = BackgroundColorToIconBaseColorMap[backgroundColor];
-
+export function Widget() {
   return (
-    <div
-      className={`container ${isMobile ? 'mobile' : 'desktop'} ${
-        sections?.length > 1 ? 'multiple-sections' : 'single-section'
-      } background-${backgroundColor}`}
-    >
-      {title && <span className="title">{title}</span>}
-      {subtitle && <span className="subtitle">{subtitle}</span>}
-      {sections &&
-        sections.map((section, key) => (
-          <div className="section" key={key}>
-            {section.title && <span className="section-title">{section.title}</span>}
-            {section.icons?.map((icon, key) => {
-              const Icon = WidgetIconMap[icon.icon];
-              const fillColor = icon.overrideDataColor ? icon.overrideDataColor : section.dataColor;
-              return (
-                <Icon
-                  key={key}
-                  data={icon.data}
-                  iconBaseColor={iconBaseColor}
-                  fillColor={fillColor}
-                />
-              );
-            })}
-          </div>
-        ))}
-      <span className="source">{source}</span>
+    <div className={`container ${isMobile ? "mobile" : "desktop"}`}>
+      timeline
     </div>
   );
 }
